e7513d3cd8b5c5b01003c466186704f3576799d7
oxmmty <oxmmty@gmail.com>
2024-10-05 23:57:10 +0900

    update check_token_owner
    Co-authored-by: oxmmty <oxmmty@gmail.com>

--- programs/marinade-finance/src/checks.rs
+++ programs/marinade-finance/src/checks.rs
@@ -114,14 +114,14 @@ pub fn check_owner_program<'info, A: ToAccountInfo<'info>>(
 //     Ok(())
 // }
 
-#[macro_export]
-macro_rules! require_lte {
-    ($value1: expr, $value2: expr, $error_code: expr $(,)?) => {
-        if $value1 > $value2 {
-            return Err(error!($error_code).with_values(($value1, $value2)));
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! require_lte {
+//     ($value1: expr, $value2: expr, $error_code: expr $(,)?) => {
+//         if $value1 > $value2 {
+//             return Err(error!($error_code).with_values(($value1, $value2)));
+//         }
+//     };
+// }
 
 #[macro_export]
 macro_rules! require_lt {
