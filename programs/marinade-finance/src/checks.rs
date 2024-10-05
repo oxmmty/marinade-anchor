833fefd693bafade420e134329b97a854230c73e
OxMMty <oxmmty@gmail.com>
2024-10-05 23:49:33 +0900

    update check_token_mint (#3)
    Co-authored: oxmmty <oxmmty@gmail.com>

--- programs/marinade-finance/src/checks.rs
+++ programs/marinade-finance/src/checks.rs
@@ -57,19 +57,19 @@ pub fn check_owner_program<'info, A: ToAccountInfo<'info>>(
 //     }
 // }
 
-pub fn check_token_mint(token: &TokenAccount, mint: &Pubkey, field_name: &str) -> Result<()> {
-    if token.mint == *mint {
-        Ok(())
-    } else {
-        msg!(
-            "Invalid token {} mint {}. Expected {}",
-            field_name,
-            token.mint,
-            mint
-        );
-        Err(Error::from(ProgramError::InvalidAccountData).with_source(source!()))
-    }
-}
+// pub fn check_token_mint(token: &TokenAccount, mint: &Pubkey, field_name: &str) -> Result<()> {
+//     if token.mint == *mint {
+//         Ok(())
+//     } else {
+//         msg!(
+//             "Invalid token {} mint {}. Expected {}",
+//             field_name,
+//             token.mint,
+//             mint
+//         );
+//         Err(Error::from(ProgramError::InvalidAccountData).with_source(source!()))
+//     }
+// }
 
 pub fn check_token_owner(token: &TokenAccount, owner: &Pubkey, field_name: &str) -> Result<()> {
     if token.owner == *owner {
