0730d87c4c8d4e8ce84c9a44502578a70a2b837e
OxMMty <oxmmty@gmail.com>
2024-10-10 00:20:20 +0900

    Dev (#23)
    * update check_token_mint
    
    Co-authored: oxmmty <oxmmty@gmail.com>
    
    * update check_token_owner
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * update check_token_owner
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * update check_token_owner
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * update macro_rule
    
    * update check_token_source_account
    
    * update check_owner_program
    
    * update var
    
    * update origin
    
    * update anchor_lang
    
    * update proportional
    
    * update owner_program
    
    * fix mint_authority
    
    * fix check_freeze_authority
    
    * fix check_mint
    
    * fix token_mint
    
    * fix token_owner
    
    * fix comment
    
    * update require_lte

--- programs/marinade-finance/src/checks.rs
+++ programs/marinade-finance/src/checks.rs
@@ -116,12 +116,12 @@ use anchor_spl::token::{Mint, TokenAccount};
 // }
 
 
-#[macro_export]
-macro_rules! require_lte {
-    ($value1: expr, $value2: expr, $error_code: expr $(,)?) => {
-        if $value1 > $value2 {
-            return Err(error!($error_code).with_values(($value1, $value2)));
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! require_lte {
+//     ($value1: expr, $value2: expr, $error_code: expr $(,)?) => {
+//         if $value1 > $value2 {
+//             return Err(error!($error_code).with_values(($value1, $value2)));
+//         }
+//     };
+// }
 
